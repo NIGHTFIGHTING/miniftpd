10d1a4d5398ef9cfe7bfbb07ff32c48c0d8d72e4
liuqi15 <liuqi15@baidu.com>
2019-05-29 20:29:46 -0700

    add miniftpd

---
+++ CMakeFiles/miniftpd.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/root/miniftpd/ftpproto.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/ftpproto.c.o"
+  "/root/miniftpd/hash.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/hash.c.o"
+  "/root/miniftpd/main.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/main.c.o"
+  "/root/miniftpd/parseconf.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/parseconf.c.o"
+  "/root/miniftpd/privparent.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/privparent.c.o"
+  "/root/miniftpd/privsock.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/privsock.c.o"
+  "/root/miniftpd/session.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/session.c.o"
+  "/root/miniftpd/str.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/str.c.o"
+  "/root/miniftpd/sysutil.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/sysutil.c.o"
+  "/root/miniftpd/tunable.c" "/root/miniftpd/CMakeFiles/miniftpd.dir/tunable.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
